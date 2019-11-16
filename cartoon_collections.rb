f72e5ebc86b1d3bc11b02a66b458f79db9c50b77
Max M <maxmcfadden88@gmail.com>
2019-11-16 06:54:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,11 @@ def summon_captain_planet(planeteer_calls)
 end
 puts summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
+
+long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
